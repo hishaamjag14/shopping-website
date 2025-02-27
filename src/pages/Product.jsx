969c056898ef4684c23a7e178c3a7f9f791a79fb
Hishaam J <a.hishaam14@gmail.com>
2025-02-27 23:31:30 +0400

    first commit

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProducts from '../components/RelatedProducts';
 const Product = () => {
 
     const { productID } = useParams();
-    const { products, currency } = useContext(ShopContext);
+    const { products, currency, addToCart } = useContext(ShopContext);
     const [productData, setProductData] = useState(false);
     const [image, setImage] = useState('')
     const [size,setSize] = useState('')
@@ -17,10 +17,7 @@ const Product = () => {
         products.map((item) => {
             if (item._id === productID) {
                 setProductData(item)
-                setImage(item.image[0])
-                console.log(item);
-                
-
+                setImage(item.image[0])          
                 return null;
             }
         })
@@ -73,7 +70,7 @@ const Product = () => {
                             ))}
                         </div>
                     </div>
-                    <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                    <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
                     <hr className='mt-8 sm:w-4/5'/>
                     <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
                             <p>100% Original product</p>
